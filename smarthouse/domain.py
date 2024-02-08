cfe74ffad7bc2dc614d70777c9715e01682b3ed1
V1S0 <vik18tor@outlook.com>
2024-02-08 13:42:45 +0100

    godt i gang

--- smarthouse/domain.py
+++ smarthouse/domain.py
@@ -1,4 +1,4 @@
-class Measurement:
+class measurement:
     """
     This class represents a measurement taken from a sensor.
     """
@@ -7,6 +7,7 @@ class Measurement:
         self.timestamp = timestamp
         self.value = value
         self.unit = unit
+
 
 
 
@@ -22,55 +23,154 @@ class SmartHouse:
     The SmartHouse class provides functionality to register rooms and floors (i.e. changing the 
     house's physical layout) as well as register and modify smart devices and their state.
     """
+        #def __init__(self,floors:[],rooms:[]) -> None:
+       # self.floors = floors
+        #self.rooms = rooms
+    
+    floors = []
+    rooms = []
+    devices = []
 
-    def register_floor(self, level):
+
+    def register_floor(self, level):#funker
         """
         This method registers a new floor at the given level in the house
         and returns the respective floor object.
         """
+        SmartHouse.floors.append(floor(level))
+        return SmartHouse.floors[-1] 
 
-    def register_room(self, floor, room_size, room_name = None):
+    def register_room(self, floor, room_size, room_name = None):#funker
         """
         This methods registers a new room with the given room areal size 
         at the given floor. Optionally the room may be assigned a mnemonic name.
         """
-        pass
+        SmartHouse.rooms.append(room(room_name,room_size,floor))
+ 
 
 
-    def get_floors(self):
+    def get_floors(self):#funker
         """
         This method returns the list of registered floors in the house.
         The list is ordered by the floor levels, e.g. if the house has 
         registered a basement (level=0), a ground floor (level=1) and a first floor 
         (leve=1), then the resulting list contains these three flors in the above order.
         """
-        pass
+        sorted_floors = SmartHouse.floors.sort()
+
+        return sorted_floors
 
 
-    def get_rooms(self):
+    def get_rooms(self):#funker
         """
         This methods returns the list of all registered rooms in the house.
         The resulting list has no particular order.
         """
-        pass
+
+        return SmartHouse.rooms
 
 
-    def get_area(self):
+    def get_area(self):#funker
         """
         This methods return the total area size of the house, i.e. the sum of the area sizes of each room in the house.
         """
+        
 
 
+        allRooms = SmartHouse.get_rooms(self)
+
+        allArea = []
+        
+        for noe in allRooms:
+            allArea.append(noe.area)
+
+
+        totalArea = sum(allArea)
+        return totalArea
+    
     def register_device(self, room, device):
         """
         This methods registers a given device in a given room.
         """
-        pass
+        SmartHouse.devices.append(room, device)
 
     
     def get_device(self, device_id):
         """
         This method retrieves a device object via its id.
         """
+        for noe in SmartHouse.devices:
+            if noe.device.device_id == device_id:
+                return noe.device
+            else:
+                return print("ingen enheter med denne id")
+
+
+
+class building:
+    def __init__(self,floors:[],rooms:[]) -> None:
+        self.floors = floors
+        self.rooms = rooms
+    
+     
+        
+
+class floor:
+    def __init__(self,floorNumber : int, ) -> None:
+        self.floorNumber = floorNumber
+
+    #def calculateArea(self, SmartHouse.rooms):
+        
         pass
+
+class room:
+    def __init__(self, name : str, area:float, floor:int) -> None:
+        self.name = name
+        self.area = area
+        self.floor = floor
+        pass
+
+class device:
+    def __init__(self, ID:int, manufacturer:str, model:str, deviceType:str, nickname:str) -> None:
+        self.ID = ID
+        self.manufacturer = manufacturer
+        self.model = model
+        self.deviceType = deviceType
+        self.nickname = nickname
+        pass
+
+class actuator(device):
+    def __init__(self, ID: int, manufacturer: str, model: str, deviceType: str, nickname: str, state) -> None:
+        super().__init__(ID, manufacturer, model, deviceType, nickname)
+        self.state = state
+    def changeState(newState):
+        #.......
+        pass
+
+class sensor(device):
+    def __init__(self, ID: int, manufacturer: str, model: str, deviceType: str, nickname: str) -> None:
+        super().__init__(ID, manufacturer, model, deviceType, nickname)
+        self.measurements = []  # Lager for å holde målinger
+
+    def getCurrentValue(self):
+        
+        if self.measurements:
+            return self.measurements[-1].value
+        else:
+            return None
+        
+    def addMeasurement(self,measurement):
+        self.measurements.append(measurement)
+
+    def getHistory(self):
+        return self.measurements
+        
+
+
+    
+    
+        
+
+    
+
 
--- tests/demo_house.py
+++ tests/demo_house.py
@@ -5,5 +5,22 @@ DEMO_HOUSE = SmartHouse()
 # Building house structure
 ground_floor = DEMO_HOUSE.register_floor(1)
 entrance = DEMO_HOUSE.register_room(ground_floor, 13.5, "Entrance")
+garage = DEMO_HOUSE.register_room(ground_floor, 19.0, "Garage")
+guestRoom1 = DEMO_HOUSE.register_room(ground_floor, 8.0, "Guest Room 1")
+Bathroom1 = DEMO_HOUSE.register_room(ground_floor, 6.3, "Bathroom 1")
+LivingRoomKitchen = DEMO_HOUSE.register_room(ground_floor, 39.75, "LivingRoom/Kitchen")
+
+first_floor = DEMO_HOUSE.register_floor(2)
+Hallway = DEMO_HOUSE.register_room(first_floor, 10.0, "Hallway")
+guestRoom2 = DEMO_HOUSE.register_room(first_floor, 8.0, "Guest Room 2")
+Bathroom2 = DEMO_HOUSE.register_room(first_floor, 9.25, "Bathroom 2")
+Office = DEMO_HOUSE.register_room(first_floor, 11.75, "Office")
+guestRoom3 = DEMO_HOUSE.register_room(first_floor, 10.0, "Guest Room 3")
+DressingRoom = DEMO_HOUSE.register_room(first_floor, 4.0, "Dressing Room")
+Masterbedroom = DEMO_HOUSE.register_room(first_floor, 17.0, "Master bedroom")
+
+#devices
+
+SmartLock = DEMO_HOUSE.register_device(entrance, )
 # TODO: continue registering the remaining floor, rooms and devices
 
